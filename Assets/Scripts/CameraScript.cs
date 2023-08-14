9ed714ede98b822164179264e8d92f56ca896093
JeanFrancoisRetief <2458318@students.wits.ac.za>
2023-08-14 10:20:38 +0200

    RawImage Setup

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraScript : MonoBehaviour
 {
+    [Header("Raw Images")]
+    public RawImage MainStage;
+    public RawImage DiningArea;
+    public RawImage BackStage;
+    public RawImage PiratesCove;
+    public RawImage Restrooms;
+    public RawImage SupplyCloset;
+    public RawImage Kitchen;
+    public RawImage WestHall;
+    public RawImage WestHallCorner;
+    public RawImage EastHall;
+    public RawImage EastHallCorner;
+
     [Header("Stage")]
     public Texture imgStageFull;
     public Texture imgStageNoB;

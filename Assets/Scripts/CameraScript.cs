155843a9d240f23fb694ae0ea729ee3595b0101e
JeanFrancoisRetief <2458318@students.wits.ac.za>
2023-08-14 10:41:02 +0200

    Script variables

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -18,15 +18,56 @@ public class CameraScript : MonoBehaviour
     public RawImage EastHall;
     public RawImage EastHallCorner;
 
-    [Header("Stage")]
+    [Header("Main Stage")]
     public Texture imgStageFull;
     public Texture imgStageNoB;
     public Texture imgStageNoBorC;
     public Texture imgStageEmpty;
+
+    [Header("Dining Area")]
+    public Texture imgDiningEmpty;
+    public Texture imgDiningBonnie;
+    public Texture imgDiningChica;
+    public Texture imgDingingBandC;
+
+    [Header("BackStage")]
+    public Texture imgBackstageEmpty;
+    public Texture imgBackstageBonnie;
+
+    [Header("Pirates Cove")]
+    public Texture imgPiratePhase1;
+    public Texture imgPiratePhase2;
+    public Texture imgPiratePhase3;
+    public Texture imgPiratePhase4;
+
+    [Header("Restrooms")]
+    public Texture imgRestroomsEmpty;
+    public Texture imgRestroomsChica;
+    public Texture imgRestroomsFreddy;
+
+    [Header("Supply Closet")]
+    public Texture imgClosetEmpty;
+    public Texture imgClosetBonnie;
+
+    //Kitchen never changes
+
+    [Header("WestHall")]
+    public Texture imgWestHallEmpty;
+    public Texture imgWestHallBonnie;
+    public Texture imgWestHallCornerEmpty;
+    public Texture imgWestHallCornerBonnie;
+
+    [Header("EastHall")]
+    public Texture imgEastHallEmpty;
+    public Texture imgEastHallChica;
+    public Texture imgEastHallCornerEmpty;
+    public Texture imgEastHallCornerChica;
+    public Texture imgEastHallCornerFreddy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

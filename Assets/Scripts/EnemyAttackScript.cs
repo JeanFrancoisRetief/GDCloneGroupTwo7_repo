5b0b3b10ddb8bfdd81a886d86ec2211528b907e1
MalakaiBraam <140538653+MalakaiBraam@users.noreply.github.com>
2023-08-19 19:28:59 +0200

    UpdatedEnemyATtack

--- Assets/Scripts/EnemyAttackScript.cs
+++ Assets/Scripts/EnemyAttackScript.cs
@@ -10,11 +10,13 @@ public class EnemyAttackScript : MonoBehaviour
 
     public int BonnieAttackCounter;
     public int ChicaAttackCounter;
+    public int FreddyAttackCounter;
     // Start is called before the first frame update
     void Start()
     {
         BonnieAttackCounter = 15 * 60;
         ChicaAttackCounter = 15 * 60;
+        FreddyAttackCounter = 20 * 60;
     }
 
     // Update is called once per frame
@@ -34,10 +36,70 @@ public class EnemyAttackScript : MonoBehaviour
                 BonnieAttackCounter = 15 * 60;
             }
         }
+
+        if ((CameraScript.ChicaLocation == CameraScript.Location.OFFICE))
+        {
+            ChicaAttackCounter--;
+            if ((ChicaAttackCounter <= 0) && !(OfficeScript.IsRightDoorClosed))
+            {
+                PlayChicaJumpScare();
+
+            }
+            else if ((OfficeScript.IsRightDoorClosed))
+            {
+                CameraScript.ChicaLocation = CameraScript.Location.DiningArea;
+                ChicaAttackCounter = 15 * 60;
+            }
+        }
+
+        if ((CameraScript.FreddyLocation == CameraScript.Location.EastHallCorner))
+        {
+            FreddyAttackCounter--;
+            if ((FreddyAttackCounter <= 0) && !(OfficeScript.IsRightDoorClosed))
+            {
+                PlayFreddyJumpScare();
+
+            }
+            else if ((OfficeScript.IsRightDoorClosed))
+            {
+                CameraScript.FreddyLocation = CameraScript.Location.MainStage;
+                FreddyAttackCounter = 20 * 60;
+            }
+        }
+
+        if ((CameraScript.FoxyLocation == CameraScript.Location.EastHallCorner))
+        {
+            FreddyAttackCounter--;
+            if ((FreddyAttackCounter <= 0) && !(OfficeScript.IsRightDoorClosed))
+            {
+                PlayFoxyJumpScare();
+
+            }
+            else if ((OfficeScript.IsRightDoorClosed))
+            {
+                CameraScript.FreddyLocation = CameraScript.Location.MainStage;
+                FreddyAttackCounter = 20 * 60;
+            }
+        }
     }
 
     public void PlayBonnieJumpScare()
     {
         SceneManager.LoadScene("BonnieJumpScare");
+    }
+    
+    public void PlayChicaJumpScare()
+    {
+        SceneManager.LoadScene("ChicaJumpScare");
+    }
+
+    public void PlayFreddyJumpScare()
+    {
+        SceneManager.LoadScene("FreddieJumpScare");
+    }
+
+    public void PlayFoxyJumpScare()
+    {
+        SceneManager.LoadScene("FoxyJumpScare");
     }
 }
